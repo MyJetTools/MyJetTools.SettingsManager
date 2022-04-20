13fd9dab587100137901376432853dd57cc80028
Hays R <wonderhays@gmail.com>
2022-04-21 00:24:37 +0300

    added client

---
+++ MyJetTools.SettingsManager.Client/SettingsReader.cs
@@ -0,0 +1,38 @@
+﻿using Flurl.Http;
+using YamlDotNet.Serialization;
+
+namespace MyJetTools.SettingsManager.Client;
+
+public class SettingsReader
+{
+    public static T GetSettings<T>(string settings = "./settings")
+    {
+        var (settingsString, path) = GetSettingsAsString(settings);
+        
+        if (string.IsNullOrEmpty(settingsString))
+            throw new Exception($"Setting not found. Path: {path}");
+
+        var deserializer = new DeserializerBuilder()
+            .IgnoreUnmatchedProperties()
+            .Build();
+
+        return deserializer.Deserialize<T>(settingsString);
+    }
+
+    private static (string, string) GetSettingsAsString(string settingsPath)
+    {
+        var envVariable = Environment.GetEnvironmentVariable("SETTINGS_URL");
+
+        if (string.IsNullOrEmpty(envVariable)) 
+            return (File.ReadAllText(settingsPath), settingsPath);
+        
+        var settingsResponse = envVariable
+            .WithClient(new FlurlClient(new HttpClient(new HttpClientHandler
+            {
+                ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true
+            })))
+            .GetStringAsync().Result;
+
+        return (settingsResponse, envVariable);
+    } 
+}
